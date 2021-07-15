68766d93a9017da11f3b65981365220412688f96
Frederic Kettelhoit <kettelhoit@gmail.com>
2021-07-15 20:20:19 +0200

    Send more requests in parallel by using async requests + try_join_all

--- tests/client.rs
+++ tests/client.rs
@@ -2,11 +2,14 @@
 
 #![cfg(not(target_arch = "wasm32"))]
 
+use futures::future::try_join_all;
+
 // Depends on running `wrangler dev` to start a worker on localhost.
 
-// The tests will (usually) fail if run in parallel using `cargo test` but
-// always pass if run using `cargo test -- --test-threads=1`, as the async
-// promise that Rust is waiting on is canceled on the next request.
+// The tests will (usually) fail if run in parallel using `cargo test parallel`
+// but always pass if run using `cargo test sequential`, as the async promise
+// that Rust is waiting on is apparently canceled if too many requests are handled in
+// parallel.
 
 const BASE_URL: &str = if cfg!(feature = "workers-localhost") {
     "http://localhost:8787"
@@ -14,53 +17,29 @@ const BASE_URL: &str = if cfg!(feature = "workers-localhost") {
     "https://cf-worker-rust-hanging-promise.assemblage.workers.dev"
 };
 
-fn run_test() -> Result<(), reqwest::Error> {
-    let resp = reqwest::blocking::get(BASE_URL)?;
-    assert_eq!(resp.status(), reqwest::StatusCode::OK);
+const NUM_REQUESTS: u32 = 100;
+
+#[tokio::test]
+async fn parallel_test() -> Result<(), reqwest::Error> {
+    let mut requests = Vec::new();
+    for _ in 0..NUM_REQUESTS {
+        requests.push(reqwest::get(BASE_URL));
+    }
+    let responses = try_join_all(requests).await?;
+    for resp in responses.into_iter() {
+        assert_eq!(resp.status(), reqwest::StatusCode::OK);
+    }
     Ok(())
 }
 
 #[test]
-fn test1() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test2() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test3() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test4() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test5() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test6() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test7() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test8() -> Result<(), reqwest::Error> {
-    run_test()
-}
-
-#[test]
-fn test9() -> Result<(), reqwest::Error> {
-    run_test()
+fn sequential_test() -> Result<(), reqwest::Error> {
+    let mut responses = Vec::new();
+    for _ in 0..NUM_REQUESTS {
+        responses.push(reqwest::blocking::get(BASE_URL)?);
+    }
+    for resp in responses.into_iter() {
+        assert_eq!(resp.status(), reqwest::StatusCode::OK);
+    }
+    Ok(())
 }
